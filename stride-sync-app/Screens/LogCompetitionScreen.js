560c636942dd72d018f226f94d752a7521bd50f3
Radite <joshuameghoo@gmail.com>
2024-08-20 22:16:57 -0500

    Add events

--- stride-sync-app/Screens/LogCompetitionScreen.js
+++ stride-sync-app/Screens/LogCompetitionScreen.js
@@ -11,9 +11,26 @@ import { startOfDay } from 'date-fns';
 const eventOptions = [
   { label: '60m', value: '60m' },
   { label: '100m', value: '100m' },
-  // ... other events
+  { label: '200m', value: '200m' },
+  { label: '400m', value: '400m' },
+  { label: '800m', value: '800m' },
+  { label: '1500m', value: '1500m' },
+  { label: '3000m', value: '3000m' },
+  { label: '5000m', value: '5000m' },
+  { label: '10000m', value: '10000m' },
+  { label: '100mH', value: '100mH' },
+  { label: '110mH', value: '110mH' },
+  { label: '3000m Steeplechase', value: '3000m Steeplechase' },
+  { label: 'Half Marathon', value: 'Half Marathon' },
+  { label: 'Marathon', value: 'Marathon' },
+  { label: 'Long Jump', value: 'Long Jump' },
+  { label: 'High Jump', value: 'High Jump' },
+  { label: 'Pole Vault', value: 'Pole Vault' },
+  { label: 'Shot Put', value: 'Shot Put' },
+  { label: 'Discus', value: 'Discus' },
+  { label: 'Javelin', value: 'Javelin' },
+  { label: 'Hammer Throw', value: 'Hammer Throw' },
 ];
-
 const LogCompetitionScreen = ({ navigation }) => {
   const [currentPBs, setCurrentPBs] = useState({});
   const today = startOfDay(new Date());
